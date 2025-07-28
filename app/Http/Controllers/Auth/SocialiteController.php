0b834f179553c81e025ecd32b5c926bc86ad2856
rodrigojp2005 <rodrigojp2005@gmail.com>
2025-07-28 18:17:37 -0300

    ajuste do botão de login do google!

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -20,20 +20,40 @@ class SocialiteController extends Controller
         try {
             $googleUser = Socialite::driver('google')->stateless()->user();
 
+            // Debug: log dados do usuário Google
+            \Log::info('Google user:', [
+                'id' => $googleUser->getId(),
+                'name' => $googleUser->getName(),
+                'email' => $googleUser->getEmail(),
+            ]);
+
             $user = User::where('email', $googleUser->getEmail())->first();
 
             if (!$user) {
-                $user = User::create([
-                    'name' => $googleUser->getName(),
-                    'email' => $googleUser->getEmail(),
-                    'password' => bcrypt(uniqid()), // senha aleatória
-                ]);
+                try {
+                    $user = User::create([
+                        'name' => $googleUser->getName() ?? $googleUser->getNickname() ?? 'Google User',
+                        'email' => $googleUser->getEmail(),
+                        'password' => bcrypt(uniqid()), // senha aleatória
+                        'email_verified_at' => now(), // marca como verificado
+                        'created_at' => now(),
+                        'updated_at' => now(),
+                    ]);
+                } catch (\Exception $e) {
+                    \Log::error('Erro ao criar usuário Google: ' . $e->getMessage());
+                    return redirect('/login')->withErrors(['msg' => 'Erro ao criar usuário Google: ' . $e->getMessage()]);
+                }
             }
 
             Auth::login($user);
-            return redirect()->intended('/');
+            if (Auth::check()) {
+                return redirect()->intended('/');
+            } else {
+                return redirect('/login')->withErrors(['msg' => 'Falha ao autenticar usuário.']);
+            }
         } catch (\Exception $e) {
-            return redirect('/login')->withErrors(['msg' => 'Erro ao autenticar com Google.']);
+            \Log::error('Erro Socialite: ' . $e->getMessage());
+            return redirect('/login')->withErrors(['msg' => 'Erro ao autenticar com Google: ' . $e->getMessage()]);
         }
     }
 }
